09085250d8d544a9dfe1522f7a6d2c8091014c9a
Shenenkaka <lvyinkids@gmail.com>
2021-06-17 23:49:20 +0800

    rebase

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  {{ msg }}
+  <span v-bind:title="message">
+    点我悬停看效果
+  </span>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  data:() => {
+    return {
+      message: 'hello, i am kaka',
+      count: 0
+    }
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+
+  mounted(){
+    console.log('mounted >>>', this.count)
+  },
+
+  increasement(){
+    console.log("this >>> ", this)
+  }
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
